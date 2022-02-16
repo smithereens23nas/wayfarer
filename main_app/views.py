d455e84511567110e1d3ce6ca12483bf6e4c8621
Smlcrp <smlcrp@gmail.com>
2022-02-16 13:42:47 -0500

    added more imports to views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.views import View, ListView
+from django.views.generic.base import View, TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
 from django.http import HttpResponse
 
 # Create your views here.
