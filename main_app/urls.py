5dfd5ab14fdca16724f09c7d29a53cbd2c431b87
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-16 18:15:58 -0500

    finished stuff

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     # path('login/', views.LoginView.as_view(), name='login'),
     path('signup/', views.ProfileCreate.as_view(), name='signup'),
-    path('profile/<int:pk>/', views.ProfileDetail.as_view(), name='profile_detail'),
-    path('profile/<int:pk>/edit/', views.ProfileEdit.as_view(), name='profile_edit'),
+    path('profile/', views.ProfileDetail.as_view(), name='profile_detail'),
+    path('profile/<int:pk>/edit/', views.ProfileEdit.as_view(), name='profile_update'),
     path('posts/', views.PostList.as_view(), name='post_list'),
     path('posts/new/', views.AddPostView.as_view(), name='add_post')
 ]
