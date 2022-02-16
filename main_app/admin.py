b88d3987b360033df3b789553c1c6ec07b19b7c2
Jason <jay.garriques@gmail.com>
2022-02-16 14:37:08 -0500

    models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+@admin.register(models.Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = [ 'title','city','img','body','author']
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','name','email','city','profile_picture']
+
+    
+# admin.site.register(models.Country)
+@admin.register(models.City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['name', 'img', 'description','post']
+    ordering = ['name']
