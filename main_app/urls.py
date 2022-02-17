c38c7920fd8b3e9a54bdbad90c6d9bb49018a7c0
Smlcrp <smlcrp@gmail.com>
2022-02-17 18:23:50 -0500

    updated urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('signup/', views.ProfileCreate.as_view(), name='signup'),
     path('profile/<int:pk>/', views.ProfileDetail.as_view(), name='profile_detail'),
+    path('profile/new/', views.ProfileCreate.as_view(), name='profile_create'),
     path('profile/<int:pk>/edit/', views.ProfileEdit.as_view(), name='profile_update'),
+    path('profile/<int:pk>/delete/', views.ProfileDelete.as_view(), name='profile_delete'),
     path('posts/', views.PostList.as_view(), name='post_list'),
     path('posts/new/', views.AddPostView.as_view(), name='add_post')
 ]
